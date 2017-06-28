7c296147b00c233cc1d024133df952d5735b7fe2
timkofu <509078+timkofu@users.noreply.github.com>
2017-06-28 12:33:50 +0300

    bugfix: setup.py keyword rep

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from distutils.core import setup
 setup(
   name = 'gitstars',
-  packages = ['gitstars'],
-  version = '1.0.2',
+  version = '1.0.3',
   description = 'A Django app to help you manage your starred GitHub projects.',
   author = 'Timothy Makobu',
   author_email = 'timkofu@gmail.com',
   url = 'https://github.com/timkofu/gitstars',
-  download_url = 'https://github.com/timkofu/gitstars/archive/1.0.2.tar.gz',
+  download_url = 'https://github.com/timkofu/gitstars/archive/1.0.3.tar.gz',
   keywords = ['python', 'django', 'github'],
   install_requires = [
     'django',
