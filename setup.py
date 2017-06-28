8367096c7aa7b6317c741f8dafecb203a96d663a
timkofu <509078+timkofu@users.noreply.github.com>
2017-06-28 11:29:41 +0300

    setup.py edits

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'gitstars',
   packages = ['gitstars'],
-  version = '1.0.0',
+  version = '1.0.1',
   description = 'A Django app to help you manage your starred GitHub projects.',
   author = 'Timothy Makobu',
   author_email = 'timkofu@gmail.com',
   url = 'https://github.com/timkofu/gitstars',
-  download_url = 'https://github.com/timkofu/gitstars/archive/1.0.0.tar.gz',
+  download_url = 'https://github.com/timkofu/gitstars/archive/1.0.1.tar.gz',
   keywords = ['python', 'django', 'github'],
   classifiers = [],
 )
