527bc96ddc94f7183c6b8d26d72add096bae4776
timkofu <makobu.mwambiriro@gmail.com>
2017-06-28 11:15:11 +0300

    Initial upload to PyPI

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'gitstars',
+  packages = ['gitstars'],
+  version = '1.0.0',
+  description = 'A Django app to help you manage your starred GitHub projects.',
+  author = 'Timothy Makobu',
+  author_email = 'timkofu@gmail.com',
+  url = 'https://github.com/timkofu/gitstars',
+  download_url = 'https://github.com/timkofu/gitstars/archive/1.0.0.tar.gz',
+  keywords = ['python', 'django', 'github'],
+  classifiers = [],
+)
