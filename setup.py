dd8728e9a82748ac30014ea0cbc42422052d06a5
timkofu <makobu.mwambiriro@gmail.com>
2017-06-28 15:59:05 +0300

    added: initialization management command

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 setup(
   name = 'gitstars',
-  version = '1.0.3',
+  version = '1.1.0',
   description = 'A Django app to help you manage your starred GitHub projects.',
   author = 'Timothy Makobu',
   author_email = 'timkofu@gmail.com',
   url = 'https://github.com/timkofu/gitstars',
-  download_url = 'https://github.com/timkofu/gitstars/archive/1.0.3.tar.gz',
+  download_url = 'https://github.com/timkofu/gitstars/archive/1.1.0.tar.gz',
   keywords = ['python', 'django', 'github'],
   install_requires = [
     'django',
@@ -15,6 +15,7 @@ setup(
   packages = [
     'gitstars',
     'gitstars.migrations',
+    'gitstars.management.commands',
   ],
   classifiers = [],
 )
