b644b6f48bfc4774461cbef1c23d5920506125d4
timkofu <509078+timkofu@users.noreply.github.com>
2017-11-13 14:55:37 +0300

    broke down admin action 'update' to 'Add', 'Update', 'delete'; bugfixes and enhancements

--- gitstars/management/commands/initialize_stars.py
+++ gitstars/management/commands/initialize_stars.py
@@ -1,41 +1,26 @@
 
 from django.conf import settings
 from django.db import IntegrityError
-from django.db.transaction import atomic
+#from django.db.transaction import atomic
 from django.core.management.base import BaseCommand, CommandError
 
 from github import Github
 
-from gitstars.models import Language, Project
+from gitstars.gitstars.models import Language, Project
+
+from gitstars.gitstars.operations import Ops
 
 
 
 class Command(BaseCommand):
     help = 'My initial GitHub stars'
 
-    @atomic
+    #@atomic
     def handle(self, *args, **options):
 
         my_github_handle = Github(settings.GH_USERNAME, settings.GH_PASSWORD)
         my_stars = my_github_handle.get_user(settings.GH_USERNAME).get_starred()
-        saved_stars = Project.objects.all()
+        ops = Ops(my_stars, None)
 
-        # Adding new stars
-        for star in my_stars:
-            if all([star.name, star.full_name, star.language, star.description,
-                star.html_url, star.stargazers_count]):
-                try:
-                    lang = Language.objects.get_or_create(name=star.language)[0]
-                    Project.objects.get_or_create( #update_or_create exists as well :)
-                    # should be faster than get_or_create
-                    #Project( <-- this breaks the atomic transaction????
-                        name=star.name,
-                        full_name=star.full_name,
-                        description=star.description,
-                        url=star.html_url,
-                        initial_stars=star.stargazers_count,
-                        current_stars=star.stargazers_count,
-                        language=lang,
-                    )
-                except IntegrityError:
-                    pass
+        # Add new stars
+        ops.add_stars({'language':Language, 'project':Project}, expected_exceptions=IntegrityError)
---
+++ gitstars/operations.py
@@ -0,0 +1,53 @@
+
+class Ops:
+
+	def __init__(self, mystars, savedstars):
+		self.mystars = mystars
+		self.savedstars = savedstars
+
+
+	def add_stars(self, models, expected_exceptions):
+		''' Add new stars '''
+		for star in self.mystars:
+			
+			# Must have a name, full name and html url
+			if not all([star.name,star.html_url]):continue
+
+			try:
+				models['project'](
+	                name=star.name,
+	                full_name=star.full_name or 'N/A',
+	                description=star.description or 'N/A',
+	                url=star.html_url,
+	                initial_stars=star.stargazers_count,
+	                current_stars=star.stargazers_count,
+	                language=models['language'].objects.get_or_create(name=star.language or 'N/A')[0]
+	            ).save()
+			except expected_exceptions:
+			    pass
+
+
+	def fallen(self):
+		''' Delete unstared projects '''
+		current_stars = {x.full_name for x in self.mystars}
+		stored_stars = {x.full_name for x in self.savedstars}
+		fallen_stars = stored_stars.difference(current_stars)
+		for fs in fallen_stars:
+		    saved_stars.get(full_name=fs).delete()
+
+
+	def update_metadata(self, expected_exceptions):
+		''' Update starcount and descriptions '''
+		for star in self.mystars:
+		    try:
+		        saved_star = self.savedstars.get(full_name=star.full_name)
+		        if saved_star:
+		            if saved_star.current_stars != star.stargazers_count:
+		                saved_star.current_stars = star.stargazers_count
+		            elif star.description and saved_star.description != star.description:
+		                saved_star.description = star.description
+		            elif star.full_name and saved_star.full_name != star.full_name:
+		                saved_star.full_name = star.full_name
+		            saved_star.save()
+		    except expected_exceptions:
+		        continue
