42463e59b2fc2d37d981462c17309de45c6f91ba
timkofu <509078+timkofu@users.noreply.github.com>
2017-11-14 09:19:26 +0300

    sub-version bump up
    new features, monitoring for a few weeks for bugs, then upload 1.3.0 to pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 setup(
   name = 'gitstars',
-  version = '1.2.1',
+  version = '1.3.0',
   description = 'A Django app to help you manage your starred GitHub projects.',
   author = 'Timothy Makobu',
   author_email = 'timkofu@gmail.com',
   url = 'https://github.com/timkofu/gitstars',
-  download_url = 'https://github.com/timkofu/gitstars/archive/1.2.1.tar.gz',
+  download_url = 'https://github.com/timkofu/gitstars/archive/1.3.0.tar.gz',
   keywords = ['python', 'django', 'github'],
   install_requires = [
     'django',
