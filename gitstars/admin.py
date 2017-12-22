2567ff33a75c2e82236d4e1aaabd0fe08626fe4a
timkofu <makobu.mwambiriro@gmail.com>
2017-12-22 21:09:49 +0300

    bugfixes 🤦 and enhancements

--- gitstars/admin.py
+++ gitstars/admin.py
@@ -1,15 +1,12 @@
 
 from django.contrib import admin
 from django.contrib.admin import site
-#from django.db.transaction import atomic
-from django.db import IntegrityError
 from django.db.models.base import ObjectDoesNotExist
 from django.conf import settings
 
 from github import Github
 
 from .models import Project, Language
-
 from .operations import Ops
 
 
@@ -50,56 +47,33 @@ class ProjectsAdmin(admin.ModelAdmin):
 
     ordering = ("-current_stars",)
 
-    my_github_handle = Github(settings.GH_USERNAME, settings.GH_PASSWORD)
-    #def current_stars(self, obj):
-    #    cstars = self.my_github_handle.get_repo(obj.full_name).stargazers_count
-    #    if cstars > obj.initial_stars:
-    #        return "{} ++".format(cstars)
-    #    else:
-    #        return "{} --".format(cstars)
-    #current_stars.short_description = 'Stars'
-
     # Admin Actions
     actions = ('fetch', 'update', 'delete')
 
-    #@atomic
+    ops = Ops(
+        Github(settings.GH_USERNAME, settings.GH_PASSWORD).get_user(settings.GH_USERNAME).get_starred(), 
+        Project.objects.all()
+    )
+
     def fetch(self, request, queryset):
-
-        my_stars = self.my_github_handle.get_user(settings.GH_USERNAME).get_starred()
-        saved_stars = Project.objects.all()
-        ops = Ops(my_stars, saved_stars)
-
         # Add
-        ops.add_stars({'language':Language, 'project':Project}, expected_exceptions=IntegrityError)
-        
+        self.ops.add_stars()
         self.message_user(request, "Fetched")
     fetch.short_description = 'Fetch'
 
 
     def update(self, request, queryset):
-
-        my_stars = self.my_github_handle.get_user(settings.GH_USERNAME).get_starred()
-        saved_stars = Project.objects.all()
-        ops = Ops(my_stars, saved_stars)
-
         # Update
-        ops.update_metadata(expected_exceptions=ObjectDoesNotExist)
-        
+        self.ops.update_metadata(expected_exceptions=ObjectDoesNotExist)
         self.message_user(request, "Updated")
     update.short_description = 'Update'
 
 
     def delete(self, request, queryset):
-
-        my_stars = self.my_github_handle.get_user(settings.GH_USERNAME).get_starred()
-        saved_stars = Project.objects.all()
-        ops = Ops(my_stars, saved_stars)
-
         # Delete
-        ops.fallen()
-        
+        self.ops.fallen()
         self.message_user(request, "Deleted")
-    delete.short_description = 'Delete'
+    delete.short_description = 'Delete Unstared'
 
 
     # Hack, so I dont have to selct records; selects all
