39847b0af9623b69e2df829d871426c02a6e2509
Timothy Makobu <timothy@makobu.io>
2020-06-08 23:52:55 +0300

    added the stars app; next: set up TDD and CD enfrustructure

--- githubstars/githubstars/urls.py
+++ githubstars/githubstars/urls.py
@@ -1,4 +1,4 @@
-"""ghstars URL Configuration
+"""githubstars URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
---
+++ githubstars/stars/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class StarsConfig(AppConfig):
+    name = 'stars'
