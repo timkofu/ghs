f80194f294c514d33a4b6101a24eae60fe9bd3a1
timkofu <509078+timkofu@users.noreply.github.com>
2020-06-08 23:52:55 +0300

    added the stars app; next: set up TDD and CD enfrustructure

--- githubstars/manage.py
+++ githubstars/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ghstars.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'githubstars.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
