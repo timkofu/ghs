96f36aca15c4f9cbe544300928277a9e4c75f47c
Timothy Makobu <timothy@makobu.io>
2020-06-10 12:55:09 +0300

    TDD infrustrcuture set up, now CD ...

--- setup.py
+++ setup.py
@@ -1,21 +1,51 @@
-from distutils.core import setup
+import os
+import subprocess
+
+from setuptools import setup, find_packages, Command
+from pipenv.project import Project
+from pipenv.utils import convert_deps_to_pip
+
+
+pfile = Project(chdir=False).parsed_pipfile
+requirements = convert_deps_to_pip(pfile['packages'], r=False)
+test_requirements = convert_deps_to_pip(pfile['dev-packages'], r=False)
+
+
+class CleanCommand(Command):
+    """Custom clean command to tidy up the project root."""
+    user_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        subprocess.check_call(
+            'rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info ./__pycache__',
+            stderr=subprocess.STDOUT,
+            stdout=subprocess.DEVNULL,
+            shell=True
+        )
+
+
 setup(
-  name = 'gitstars',
-  version = '1.3.0',
-  description = 'A Django app to help you manage your starred GitHub projects.',
-  author = 'Timothy Makobu',
-  author_email = 'timkofu@gmail.com',
-  url = 'https://github.com/timkofu/gitstars',
-  download_url = 'https://github.com/timkofu/gitstars/archive/1.3.0.tar.gz',
-  keywords = ['python', 'django', 'github'],
-  install_requires = [
-    'django',
-    'pygithub'
-  ],
-  packages = [
-    'gitstars',
-    'gitstars.migrations',
-    'gitstars.management.commands',
-  ],
-  classifiers = [],
+    
+    name="Githubstars",
+    version="2.0",
+    author="Timothy Makobu",
+    description="Get insight into your stared projects",
+    packages=find_packages(),  # This needs __init__.py in each package (folder)
+    
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: POSIX",
+        "Environment :: Web Environment"
+    ],
+
+    python_requires=">=3.8",
+    install_requires=[requirements, test_requirements],
+    
+    cmdclass={
+        'clean': CleanCommand,
+    }
+
 )
