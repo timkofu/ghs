a25bab345dced32f1e479249c8d390c8743bd8bd
Timothy Makobu <timothy@makobu.io>
2020-12-20 15:37:42 +0300

    Initial Model

---
+++ ghs/model/database/database.py
@@ -0,0 +1,44 @@
+
+import asyncpg
+from typing import Tuple
+
+
+class Database:
+    """ Database interface """
+
+    __slots__ = (
+        "db_handle",
+    )
+
+    # Give db handle in constructor instead of creating it within to help
+    # give a mock object when testing
+    def __init__(self, db_handle: asyncpg.connection.Connection) -> None:
+        self.db_handle = db_handle
+
+    async def create(self, query: Tuple[str, ...]) -> asyncpg.Record:
+
+        if not query[0].startswith('INSERT'):
+            raise ValueError("Not an INSERT query")
+
+        return await self.db_handle.execute(*query)
+
+    async def read(self, query: str) -> asyncpg.Record:
+
+        if not query.startswith('SELECT'):
+            raise ValueError("Not a SELECT query")
+
+        return await self.db_handle.fetch(query)
+
+    async def update(self, query: str) -> asyncpg.Record:
+
+        if not query.startswith('UPDATE'):
+            raise ValueError("Not an UPDATE query")
+
+        return await self.db_handle.execute(query)
+
+    async def delete(self, query: str) -> asyncpg.Record:
+
+        if not query.startswith('DELETE'):
+            raise ValueError("Not a DELETE query")
+
+        return await self.db_handle.execute(query)
---
+++ tests/model/database/test_database.py
@@ -0,0 +1,60 @@
+
+import os
+from functools import partial
+
+import asyncpg
+
+from ghs.model.database.database import Database
+
+import pytest
+pytestmark = pytest.mark.asyncio
+
+
+@pytest.mark.skipif(
+    os.getenv('CI'),  # type: ignore
+    reason="No PostgreSQL DB on GH Actions CI/CD"
+)
+class TestDatabase:
+
+    # A fresh connection is made for each test, and thats fine for now
+    conn = partial(asyncpg.connect, user="testdb", password="testdb", database="testdb")
+
+    async def test_create(self) -> None:
+
+        dbh = Database(db_handle=await self.conn())
+
+        result = await dbh.create(('INSERT INTO pro_lang(name) VALUES($1)', 'Python'))
+        assert result == "INSERT 0 1"
+
+        with pytest.raises(ValueError):
+            await dbh.create(('SELECT * FROM pro_lang',))
+
+    async def test_read(self) -> None:
+
+        dbh = Database(db_handle=await self.conn())
+
+        result = await dbh.read("SELECT name from pro_lang")
+        assert result[0].get('name') == 'Python'
+
+        with pytest.raises(ValueError):
+            await dbh.read("INSERT INTO pro_lang(name) VALUES('Javascript')")
+
+    async def test_update(self) -> None:
+
+        dbh = Database(db_handle=await self.conn())
+
+        result = await dbh.update("UPDATE pro_lang SET name='Rust' WHERE name='Python'")
+        assert result == 'UPDATE 1'
+
+        with pytest.raises(ValueError):
+            await dbh.update('SELECT name FROM pro_lang')
+
+    async def test_delete(self) -> None:
+
+        dbh = Database(db_handle=await self.conn())
+
+        result = await dbh.delete('DELETE FROM pro_lang')
+        assert result == 'DELETE 1'
+
+        with pytest.raises(ValueError):
+            await dbh.delete('SELECT name FROM pro_lang')
