2991a4531d489260f4451b4f8248bab6a6130e3a
Timothy Makobu <timothy@makobu.dev>
2021-01-17 18:21:14 +0300

    Goodbye Python

--- tests/view/web/test_cron.py
+++ tests/view/web/test_cron.py
@@ -1,10 +1,13 @@
 
+import os
+
 from starlette.testclient import TestClient
 
 from ghs.view.web.endpoints import app
 
 import pytest
 import nest_asyncio
+from pytest_mock import module_mocker, MockerFixture
 
 nest_asyncio.apply()
 pytestmark = pytest.mark.asyncio
@@ -16,10 +19,10 @@ class TestCron:
 
     async def test_fetch(self) -> None:
 
-        response = self.client.get('/fetch')
-        assert response.status_code == 200
+        response = self.client.get("/fetch/test")
+        assert response.status_code == 403
 
     async def test_update(self) -> None:
 
-        response = self.client.get('/update')
+        response = self.client.get("/update/test")
         assert response.status_code == 200
