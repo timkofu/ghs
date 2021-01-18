900f8d44c68a557252f6271fe99e34b914cead1a
Timothy Makobu <timothy@makobu.dev>
2021-01-18 17:03:33 +0300

    Hi Python. Beginning fetch functionality debugging

--- ghs/model/database/database.py
+++ ghs/model/database/database.py
@@ -4,7 +4,10 @@ import asyncio
 from types import CoroutineType
 
 import asyncpg
-from typing import Any, Tuple
+import nest_asyncio
+from typing import Tuple
+
+nest_asyncio.apply()
 
 
 class Database:
@@ -14,51 +17,42 @@ class Database:
         "db_handle",
     )
 
-    @staticmethod
-    def run_and_get(coro: CoroutineType) -> Any:
-        task: Any = asyncio.create_task(coro) # type: ignore
-        asyncio.get_running_loop().run_until_complete(task)
-        return task.result()
-
-    # Give db handle in constructor instead of creating it within to help
-    # give a mock object when testing
     def __init__(
         self,
-        db_handle: asyncpg.connection.Connection = asyncpg.connect(os.getenv('DATABASE_URL'))
+        db_handle: asyncpg.connection.Connection = asyncio.get_event_loop().run_until_complete(
+            asyncpg.connect(os.getenv('DATABASE_URL'))
+        )  # candidate for the optimization stage (should work with one loop)
     ) -> None:
-        self.db_handle = asyncio.run_coroutine_threadsafe(
-            db_handle,
-            asyncio.get_event_loop()
-        ).result()
+        self.db_handle = db_handle
 
     async def create(self, query: Tuple[str, ...]) -> asyncpg.Record:
 
         if not query[0].startswith('INSERT'):
             raise ValueError("Not an INSERT query")
 
-        async with self.db_handle.transaction():
-            return await self.db_handle.execute(*query)
+        # async with self.db_handle.transaction():
+        return await self.db_handle.execute(*query)
 
     async def read(self, query: str) -> asyncpg.Record:
 
         if not query.startswith('SELECT'):
             raise ValueError("Not a SELECT query")
 
-        async with self.db_handle.transaction():
-            return await self.db_handle.fetch(query)
+        # async with self.db_handle.transaction():
+        return await self.db_handle.fetch(query)
 
     async def update(self, query: str) -> asyncpg.Record:
 
         if not query.startswith('UPDATE'):
             raise ValueError("Not an UPDATE query")
 
-        async with self.db_handle.transaction():
-            return await self.db_handle.execute(query)
+        # async with self.db_handle.transaction():
+        return await self.db_handle.execute(query)
 
     async def delete(self, query: str) -> asyncpg.Record:
 
         if not query.startswith('DELETE'):
             raise ValueError("Not a DELETE query")
 
-        async with self.db_handle.transaction():
-            return await self.db_handle.execute(query)
+        # async with self.db_handle.transaction():
+        return await self.db_handle.execute(query)
