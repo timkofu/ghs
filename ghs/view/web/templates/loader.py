dd9aaa664cf45f34ac77c4ac92ae847a8cf102b1
Tim <tim.makobu@gmail.com>
2021-02-22 12:18:26 +0300

    deps update; tweaks

--- ghs/view/web/templates/loader.py
+++ ghs/view/web/templates/loader.py
@@ -4,6 +4,6 @@ from pathlib import Path
 from starlette.templating import Jinja2Templates
 
 
-templates_handle = Jinja2Templates(
+templates_handle: Jinja2Templates = Jinja2Templates(
     directory=Path(__file__).resolve().parent.as_posix()
 )
--- ghs/view/web/update.py
+++ ghs/view/web/update.py
@@ -10,11 +10,11 @@ from starlette.responses import PlainTextResponse
 from ghs.controller.stars.update import Update
 
 
-class UpdateStars(HTTPEndpoint):  # type: ignore
+class UpdateStars(HTTPEndpoint):
 
     async def get(self, request: Request) -> PlainTextResponse:
 
-        cron_auth_token: str = request.path_params.get('update_auth_token')
+        cron_auth_token: str = str(request.path_params.get('update_auth_token'))
 
         if cron_auth_token and (cron_auth_token == os.getenv('UPDATE_AUTH_TOKEN')):
             return PlainTextResponse("1", background=BackgroundTask(Update().stars))
