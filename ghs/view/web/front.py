5c939a8ced7d2b4c61a26d1d1cf1d0dd990bfee6
timkofu <509078+timkofu@users.noreply.github.com>
2021-04-10 15:56:44 +0300

    frontpage aesthetics et al updates

--- ghs/view/web/front.py
+++ ghs/view/web/front.py
@@ -1,4 +1,4 @@
-
+from typing import Any
 from starlette.requests import Request
 from starlette.endpoints import HTTPEndpoint
 from starlette.templating import _TemplateResponse
@@ -9,17 +9,17 @@ from .templates.loader import templates_handle
 
 
 class Front(HTTPEndpoint):
-
     async def get(self, request: Request) -> _TemplateResponse:
 
         pager = Pager()
 
         rows = [r async for r in pager.page()]
+        page_details: dict[str, Any] = {
+            "current_page": 1,
+            "last_page": 1,
+        }
 
         return templates_handle.TemplateResponse(
-            'front.html',
-            context={
-                'rows': rows,
-                'request': request
-            }
+            "front.html",
+            context={"rows": rows, "page_details": page_details, "request": request},
         )
