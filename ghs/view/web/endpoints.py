ad1f79b16a0e6c14a617bea83fc710d37140c0a6
timkofu <509078+timkofu@users.noreply.github.com>
2021-06-09 18:15:47 +0300

    Added GZipMiddleware

--- ghs/view/web/endpoints.py
+++ ghs/view/web/endpoints.py
@@ -1,5 +1,7 @@
 from starlette.routing import Route
 from starlette.applications import Starlette
+from starlette.middleware import Middleware
+from starlette.middleware.gzip import GZipMiddleware
 
 from .front import Front
 from .update import UpdateStars
@@ -16,4 +18,5 @@ app = Starlette(
         Route("/update/{update_auth_token:str}", UpdateStars),
         Route("/page/{wanted_page:int}", Front),
     ],
+    middleware=[Middleware(GZipMiddleware, minimum_size=512)],
 )
