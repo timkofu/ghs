b46a12f98aa4bfd24666637a992fb21dd93bc744
timkofu <509078+timkofu@users.noreply.github.com>
2021-06-26 17:55:14 +0300

    readme grammar fix

--- ghs/__init__.py
+++ ghs/__init__.py
@@ -1,8 +1,9 @@
 """
-GHS
+# **Git Hub Star**
 
-A web-app to help analyse my GitHub starred repos.
-My web-dev research project.
+A web-app to help analyse GitHub repos I've starred.
+
+My web-development research project.
 
 **Architecture**: [M(DDD)VC](https://blog.fedecarg.com/2009/03/11/domain-driven-design-and-mvc-architectures/).
 """
--- ghs/test_placeholder.py
+++ ghs/test_placeholder.py
@@ -1,2 +1,2 @@
 def test_placeholder() -> None:
-    assert 1 == 1
+    ...
