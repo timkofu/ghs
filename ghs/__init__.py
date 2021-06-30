d9716ed4f5979007b2960efb65670310afcdd3c9
timkofu <509078+timkofu@users.noreply.github.com>
2021-06-30 16:50:42 +0300

    Added SQLAlchemy (for the already implemented Repository and UnitOfWork patterns) and Alembic (for migrations)

--- ghs/__init__.py
+++ ghs/__init__.py
@@ -3,7 +3,7 @@
 
 A web-app to help analyse GitHub repos I've starred.
 
-My web-development research project.
+My Software Engineering research project.
 
 **Architecture**: [M(DDD)VC](https://blog.fedecarg.com/2009/03/11/domain-driven-design-and-mvc-architectures/).
 """
