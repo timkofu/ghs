d9d7637de6d5826ef4a0dd0b36e2af79b7c2372f
Tim <tim.makobu@gmail.com>
2021-07-10 19:02:09 +0300

    M(DDD)VC: preogressing ...

--- ghs/model/infrastructure/repository/model.py
+++ ghs/model/infrastructure/repository/model.py
@@ -1,3 +1,17 @@
 """
 SQL Alchemy ORM models.
+
+This will have no tests as it's pure SQLAlchemy, hich is itself tested.
 """
+
+from sqlalchemy.orm import declarative_base  # type: ignore
+from sqlalchemy import Column, String, Integer, DateTime
+
+Base = declarative_base()  # type: ignore
+
+
+class Project(Base):  # type: ignore
+
+    __tablename__ = "projects"
+
+    id = Column(Integer, primary_key=True)
