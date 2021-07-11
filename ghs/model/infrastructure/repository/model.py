4da67c52a9a75a6c0fded5cd4dbc77681045e465
timkofu <509078+timkofu@users.noreply.github.com>
2021-07-11 22:59:48 +0300

    Initial SQLAlchemy models

--- ghs/model/infrastructure/repository/model.py
+++ ghs/model/infrastructure/repository/model.py
@@ -4,14 +4,31 @@ SQL Alchemy ORM models.
 This will have no tests as it's pure SQLAlchemy, hich is itself tested.
 """
 
+import datetime
+
 from sqlalchemy.orm import declarative_base  # type: ignore
 from sqlalchemy import Column, String, Integer, DateTime
+
 
 Base = declarative_base()  # type: ignore
 
 
 class Project(Base):  # type: ignore
 
-    __tablename__ = "projects"
+    __tablename__ = "project"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, index=True)
+    description = Column(String)
+    url = Column(String)
+    stars = Column(Integer)
+    forks = Column(Integer)
+    added_on = Column(DateTime, default=datetime.datetime.utcnow)
+
+
+class ProgrammingLanguage(Base):  # type: ignore
+
+    __tablename__ = "programming_language"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, index=True)
