f259d1c959bc26650da6803ab7a709b1eae65897
timkofu <509078+timkofu@users.noreply.github.com>
2021-07-11 23:37:57 +0300

    Initial Alembic migration

--- ghs/__init__.py
+++ ghs/__init__.py
@@ -6,4 +6,5 @@ A web-app to help analyse GitHub repos I've starred.
 My Software Engineering research project.
 
 **Architecture**: [M(DDD)VC](https://blog.fedecarg.com/2009/03/11/domain-driven-design-and-mvc-architectures/).
+Datastructure exchanged between layers: Dictionary.
 """
